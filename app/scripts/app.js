f4c6c755503049ce1f7e8012189a6b9246e9c473
Axel <axel.salmeron@gmail.com>
2015-03-03 00:32:11 -0430

    Directivas y controladores
    Se ha hecho un pequeño modelo como lista de contactos para mostrar
    directivas en angularjs

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,19 @@
 'use strict';
-var app = angular.module('angularFromScratch', []);
+var app = angular.module('angularFromScratch', [
+	'ngSanitize'
+]);
 
-app.controller('Principal',['$scope',function($scope){
+app.controller('PrincipalCtrl',['$scope',function($scope){
 	this.tituloApp = 'Hola AngularJS';
+}]);
+
+app.controller('ContactosCtrl',['$scope',function($scope){
+	this.contactosArray = [
+		{nombre: 'Axel', apellido: 'Salmeron', contacto: '@axulsr', thumb: '<span class="glyphicon glyphicon-user"></span>'},
+		{nombre: 'Guillermo', apellido: 'Seminario', contacto: '@gjseminario', thumb: '<span class="glyphicon glyphicon-user"></span>'},
+		{nombre: 'Alexander', apellido: 'Rondón', contacto: '@arondn2', thumb: '<span class="glyphicon glyphicon-user"></span>'},
+		{nombre: 'José', apellido: 'Salazar', contacto: '@jslzrt', thumb: '<span class="glyphicon glyphicon-user"></span>'},
+		{nombre: 'Marlys', apellido: 'Villafranca', contacto: '@MarlysitaV', thumb: '<span class="glyphicon glyphicon-user"></span>'},
+		{nombre: 'Katrina', apellido: 'Ortiz', contacto: '@KatrinaOrtiz', thumb: '<span class="glyphicon glyphicon-user"></span>'}
+	];
 }]);
