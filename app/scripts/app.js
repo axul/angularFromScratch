0e4778fea1792061d482f133fbdeb7b1f0e5eb03
Axel <axel.salmeron@gmail.com>
2015-06-11 06:33:54 -0430

    Creación de módulo de controladores
    se crea un módulo para los controladores y se inyecta en el módulo
    principal

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,51 +1,5 @@
 'use strict';
 var app = angular.module('angularFromScratch', [
-	'ngSanitize'
+	'ngSanitize',
+	'app.controladores'
 ]);
-
-app.controller('PrincipalCtrl',['$scope',function($scope){
-	this.tituloApp = 'Hola AngularJS';
-}]);
-
-app.controller('ContactosCtrl',['$scope',function($scope){
-	this.perfilActivo = -1;
-
-	this.contactosArray = [
-		{nombre: 'Axel', apellido: 'Salmeron', contacto: '@axulsr',
-		icon: '<span class="glyphicon glyphicon-user"></span>',
-		thumb:''},
-
-		
-		{nombre: 'Guillermo', apellido: 'Seminario', contacto: '@gjseminario',
-		icon: '<span class="glyphicon glyphicon-user"></span>',
-		thumb:''},		
-		
-		{nombre: 'Alexander', apellido: 'Rondón', contacto: '@arondn2',
-		icon: '<span class="glyphicon glyphicon-user"></span>',
-		thumb:''},
-		
-		{nombre: 'José', apellido: 'Salazar', contacto: '@jslzrt',
-		icon: '<span class="glyphicon glyphicon-user"></span>',
-		thumb:''},
-		
-		{nombre: 'Marlys', apellido: 'Villafranca', contacto: '@MarlysitaV',
-		icon: '<span class="glyphicon glyphicon-user"></span>',
-		thumb:''},
-		
-		{nombre: 'Katrina', apellido: 'Ortiz', contacto: '@KatrinaOrtiz',
-		icon: '<span class="glyphicon glyphicon-user"></span>',
-		thumb:''}
-	];
-
-	this.mostrarPerfil = function(indice){
-		this.perfilActivo = indice;
-	};
-
-	this.isPerfilActivo = function(){
-		return (this.perfilActivo==-1) ?  false : true;
-	};
-}]);
-
-app.controller('PerfilCtrl',['$scope',function($scope){
-	this.perfil = '';
-}]);
---
+++ app/scripts/controladores.js
@@ -0,0 +1,46 @@
+'use strict';
+angular.module('app.controladores', [])
+.controller('PrincipalCtrl',['$scope',function($scope){
+	this.tituloApp = 'Hola AngularJS';
+}])
+.controller('ContactosCtrl',['$scope',function($scope){
+	this.perfilActivo = -1;
+
+	this.contactosArray = [
+		{nombre: 'Axel', apellido: 'Salmeron', contacto: '@axulsr',
+		icon: '<span class="glyphicon glyphicon-user"></span>',
+		thumb:''},
+
+		
+		{nombre: 'Guillermo', apellido: 'Seminario', contacto: '@gjseminario',
+		icon: '<span class="glyphicon glyphicon-user"></span>',
+		thumb:''},		
+		
+		{nombre: 'Alexander', apellido: 'Rondón', contacto: '@arondn2',
+		icon: '<span class="glyphicon glyphicon-user"></span>',
+		thumb:''},
+		
+		{nombre: 'José', apellido: 'Salazar', contacto: '@jslzrt',
+		icon: '<span class="glyphicon glyphicon-user"></span>',
+		thumb:''},
+		
+		{nombre: 'Marlys', apellido: 'Villafranca', contacto: '@MarlysitaV',
+		icon: '<span class="glyphicon glyphicon-user"></span>',
+		thumb:''},
+		
+		{nombre: 'Katrina', apellido: 'Ortiz', contacto: '@KatrinaOrtiz',
+		icon: '<span class="glyphicon glyphicon-user"></span>',
+		thumb:''}
+	];
+
+	this.mostrarPerfil = function(indice){
+		this.perfilActivo = indice;
+	};
+
+	this.isPerfilActivo = function(){
+		return (this.perfilActivo==-1) ?  false : true;
+	};
+}])
+.controller('PerfilCtrl',['$scope',function($scope){
+	this.perfil = '';
+}]);
